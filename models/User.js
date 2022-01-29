c572f9951e307e6dc2193b5b6ed322304e5e47d3
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-01-29 12:16:34 +0100

    setup the User database schema

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true,'Please provide a name'],
+        minlength: 3,
+        maxlength:50,
+    },
+    email:{
+        type: String,
+        required: [true, 'Please provide an email address'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'],
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 6,
+        maxlength: 12,
+    }
+})
+
+
+module.exports = mongoose.model('Users', UserSchema)
