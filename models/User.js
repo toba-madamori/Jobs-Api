699a5aa29aedc68f809419de986830558a0efdbb
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-01-29 13:02:15 +0100

    hashing in the controller

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -17,8 +18,13 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide a password'],
         minlength: 6,
-        maxlength: 12,
     }
+})
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 
