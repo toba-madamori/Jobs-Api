c3d52f5df4044e56790e6fc1dc2e652060e06ab8
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-01-31 23:30:29 +0100

    added the get-job, update-job and delete-job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,7 +9,14 @@ const getAllJobs = async (req,res)=>{
 }
 
 const getJob = async (req,res)=>{
-    res.send('get a particular job')
+    // getting the userid and the jobid 
+    const {user:{userID}, params:{id:jobID}} = req
+
+    const job = await Job.findOne({_id:jobID, createdBy:userID})
+    if(!job){
+        throw new NotFoundError(`No job with id:${jobID}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req,res)=>{
@@ -21,11 +28,29 @@ const createJob = async (req,res)=>{
 }
 
 const updateJob = async (req,res)=>{
-    res.send('update job')
+    const {body:{company, position}, user:{userID}, params:{id:jobID}} = req
+    // checking if values where provided for company $ position
+    if(company === '' || position === ''){
+        throw new BadRequestError('Company and Position fields cannot be empty')
+    }
+
+    const job = await Job.findByIdAndUpdate({_id:jobID, createdBy:userID}, {company, position}, {new:true, 
+    runValidators:true})
+    // checking if the job exists
+    if(!job){
+        throw new NotFoundError(`No job with the id:${ jobID }`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req,res)=>{
-    res.send('delete job')
+    const {user:{userID}, params:{id:jobID}} = req
+
+    const job = await Job.findByIdAndDelete({_id:jobID, createdBy:userID})
+    if(!job){
+        throw new NotFoundError(`No job with the id:${jobID}`)
+    }
+    res.status(StatusCodes.OK).json({msg:"success",})
 }
 
 module.exports = {
--- routes/jobs.js
+++ routes/jobs.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const {getAllJobs,getJob,createJob,updateJob,deleteJob} = require('../controllers/jobs')
 
 router.route('/').get(getAllJobs).post(createJob)
-router.route('/:id').get(getJob).post(updateJob).post(deleteJob)
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
 
 
 module.exports = router
