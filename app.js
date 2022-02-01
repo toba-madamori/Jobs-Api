28fece623e288fa652dec1f94ac61ba2631be4ae
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-02-01 14:20:34 +0100

    added security packages in preparation for deployment

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // auth handler
 const authenticateUser = require('./middleware/authentication')
 
@@ -18,7 +24,17 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // extra packages
+app.set('trust proxy', 1)
+app.use(rateLimiter(
+  {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  }  
+))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
